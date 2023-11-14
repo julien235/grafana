2f223046ee974651c3c7618849f91ee426e81410
Matias Chomicki <matyax@gmail.com>
2023-11-14 19:41:28 +0100

    Logs: remove dynamic logs container size
    It works very well with 1 query, but breaks with more than 1 query or when Logs is not the last rendered panel

--- public/app/features/explore/Logs/Logs.tsx
+++ public/app/features/explore/Logs/Logs.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import { capitalize, throttle } from 'lodash';
+import { capitalize } from 'lodash';
 import memoizeOne from 'memoize-one';
 import React, { createRef, PureComponent } from 'react';
 
@@ -174,12 +174,6 @@ class UnthemedLogs extends PureComponent<Props, State> {
     if (this.cancelFlippingTimer) {
       window.clearTimeout(this.cancelFlippingTimer);
     }
-
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
   }
 
   updatePanelState = (logsPanelState: Partial<ExploreLogsPanelState>) => {
@@ -226,23 +220,8 @@ class UnthemedLogs extends PureComponent<Props, State> {
     }
   };
 
-  handleResize = throttle(() => {
-    if (!this.state.logsContainer || !scrollableLogsContainer) {
-      return;
-    }
-    this.setState({ logRowsHeight: this.calculateLogRowsHeight(this.state.logsContainer) });
-  }, 100);
-
-  calculateLogRowsHeight = (node: HTMLDivElement) => {
-    const rect = node.getBoundingClientRect();
-    return window.innerHeight - rect.y; 
-  }
-
   onLogsContainerRef = (node: HTMLDivElement) => {
     const newState = { logsContainer: node, logRowsHeight: this.state.logRowsHeight };
-    if (scrollableLogsContainer) {
-      newState.logRowsHeight = this.calculateLogRowsHeight(node); 
-    }
     this.setState(newState);
   };
 
@@ -770,7 +749,7 @@ class UnthemedLogs extends PureComponent<Props, State> {
               </div>
             )}
             {this.state.visualisationType === 'logs' && hasData && (
-              <div className={scrollableLogsContainer ? styles.scrollableLogRows : styles.logRows} data-testid="logRows" ref={this.onLogsContainerRef} style={{ height: scrollableLogsContainer ? this.state.logRowsHeight : 'auto' }}>
+              <div className={scrollableLogsContainer ? styles.scrollableLogRows : styles.logRows} data-testid="logRows" ref={this.onLogsContainerRef}>
                 <LogRows
                   logRows={logRows}
                   deduplicatedRows={dedupedRows}
@@ -885,6 +864,7 @@ const getStyles = (theme: GrafanaTheme2, wrapLogMessage: boolean) => {
       overflow-x: scroll;
       overflow-y: visible;
       width: 100%;
+      max-height: calc(100vh - 170px);
     `,
     logRows: css`
       overflow-x: ${wrapLogMessage ? 'unset' : 'scroll'};
